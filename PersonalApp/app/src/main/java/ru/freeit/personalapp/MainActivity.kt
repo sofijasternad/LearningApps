a26d22b6f7f14260c325367b564f8a37936494ef
Dmitry <dmitry.tehnicov@gmail.com>
2021-10-24 14:16:43 +0600

    создание приложения PersonalApp

---
+++ PersonalApp/app/src/main/java/ru/freeit/personalapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.freeit.personalapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
