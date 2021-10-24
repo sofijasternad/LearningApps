cdc7768f5d1a3cf2f7ea600bcaab95ffdc7d99e1
Dmitry <dmitry.tehnicov@gmail.com>
2021-10-24 14:35:15 +0600

    добавление обработчика события OnClickListener на кнопку Подписаться

--- PersonalApp/app/src/main/java/ru/freeit/personalapp/MainActivity.kt
+++ PersonalApp/app/src/main/java/ru/freeit/personalapp/MainActivity.kt
@@ -1,7 +1,10 @@
 package ru.freeit.personalapp
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,5 +12,11 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         supportActionBar?.title = Name(this).str()
+
+        val root = findViewById<View>(R.id.root)
+
+        findViewById<Button>(R.id.subscribe_button).setOnClickListener {
+            Snackbar.make(root, R.string.subscribed, Snackbar.LENGTH_SHORT).show()
+        }
     }
 }
