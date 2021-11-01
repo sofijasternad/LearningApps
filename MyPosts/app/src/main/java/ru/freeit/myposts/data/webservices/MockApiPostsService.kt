912970f4b98bc5de8267d92451b6f4f822f5309e
Dmitry <dmitry.tehnicov@gmail.com>
2021-11-01 10:14:52 +0600

    добавление кода в MockApiPostsService

--- MyPosts/app/src/main/java/ru/freeit/myposts/data/webservices/MockApiPostsService.kt
+++ MyPosts/app/src/main/java/ru/freeit/myposts/data/webservices/MockApiPostsService.kt
@@ -1,4 +1,43 @@
 package ru.freeit.myposts.data.webservices
 
+import org.json.JSONArray
+import org.json.JSONObject
+import ru.freeit.myposts.data.model.Post
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.lang.StringBuilder
+import java.net.URL
+import javax.net.ssl.HttpsURLConnection
+
 class MockApiPostsService {
+
+    private fun JSONArray.mapJsonObjects() : List<JSONObject> {
+        val jsonObjects = mutableListOf<JSONObject>()
+        for (i in 0 until length()) {
+            jsonObjects.add(getJSONObject(i))
+        }
+        return jsonObjects
+    }
+
+    private val mockApiPostsUrl = "https://617f607b055276001774fa5f.mockapi.io/api/v3/posts"
+
+    fun fetchPosts() : List<Post> {
+        val connection = URL(mockApiPostsUrl).openConnection() as HttpsURLConnection
+        connection.requestMethod = "GET"
+        connection.setRequestProperty("Accept", "application/json")
+        BufferedReader(InputStreamReader(connection.inputStream, "utf-8")).use {  reader ->
+            val jsonContent = StringBuilder()
+            var line: String? = reader.readLine()
+            while (line != null) {
+                jsonContent.append(line.trim())
+                line = reader.readLine()
+            }
+            connection.disconnect()
+            val postsJson = JSONArray(jsonContent.toString())
+            return postsJson.mapJsonObjects().map { postJson ->
+                Post.fromJson(postJson)
+            }
+        }
+    }
+
 }
