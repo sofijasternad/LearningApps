32f023317f8b1e82f87c5c282503386d1375cdb1
Dmitry <dmitry.tehnicov@gmail.com>
2021-11-01 10:02:57 +0600

    создание модели Post

--- MyPosts/app/src/main/java/ru/freeit/myposts/data/model/Post.kt
+++ MyPosts/app/src/main/java/ru/freeit/myposts/data/model/Post.kt
@@ -1,4 +1,38 @@
 package ru.freeit.myposts.data.model
 
-class Post {
+import org.json.JSONObject
+
+data class Post(
+    val id: String,
+    val createdAt: String,
+    val title: String,
+    val content: String,
+    val imageUrl: String
+) {
+
+    private val timestampDelimiter = "T"
+    private val dateDelimiter = "-"
+
+    fun date() : String {
+        val (date, _) = createdAt.split(timestampDelimiter)
+        val dateSlices = date.split(dateDelimiter)
+        return "${dateSlices.last()}.${dateSlices[1]}.${dateSlices.first()}"
+    }
+
+    fun title() = title
+    fun image() = imageUrl
+    fun text() = content
+
+    companion object {
+        fun fromJson(json: JSONObject) : Post {
+            return Post(
+                json.getString("id"),
+                json.getString("createdAt"),
+                json.getString("title"),
+                json.getString("content"),
+                json.getString("image")
+            )
+        }
+    }
+
 }
