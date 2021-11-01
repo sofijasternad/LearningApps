596af4f2ca82428bd34f0391f171b6d663378c13
Dmitry <dmitry.tehnicov@gmail.com>
2021-11-01 15:57:42 +0600

    добавление webview с поддержкой JavaScript и установки иконки для приложения

--- AliExpress/app/src/main/java/ru/freeit/aliexpress/MainActivity.kt
+++ AliExpress/app/src/main/java/ru/freeit/aliexpress/MainActivity.kt
@@ -1,16 +1,47 @@
 package ru.freeit.aliexpress
 
+import android.opengl.Visibility
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.webkit.WebResourceRequest
 import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.core.view.isVisible
+import com.google.android.material.progressindicator.CircularProgressIndicator
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var webView: WebView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val webView = findViewById<WebView>(R.id.ali_express_web_view)
-        webView.loadUrl("aliexpress.ru")
+        webView = findViewById(R.id.ali_express_web_view)
+        val progressPageLoading = findViewById<CircularProgressIndicator>(R.id.progress_page_loading)
+        webView.webViewClient = object: WebViewClient() {
+            override fun onPageFinished(view: WebView?, url: String?) {
+                super.onPageFinished(view, url)
+                progressPageLoading.visibility = View.GONE
+            }
+        }
         webView.settings.javaScriptEnabled = true
+        webView.loadUrl("https://best.aliexpress.ru/")
+    }
+
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+        if (event?.action == KeyEvent.ACTION_DOWN) {
+            if (keyCode == KeyEvent.KEYCODE_BACK) {
+                if (webView.canGoBack()) {
+                    webView.goBack()
+                } else {
+                    finish()
+                }
+                return true
+            }
+        }
+        return super.onKeyDown(keyCode, event)
     }
 }
