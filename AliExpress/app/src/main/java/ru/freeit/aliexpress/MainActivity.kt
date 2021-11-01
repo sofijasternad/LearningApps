1fbbde26592c64e1350ef9330f3c55dbf57f3086
Dmitry <dmitry.tehnicov@gmail.com>
2021-11-01 15:19:54 +0600

    добавление приложения AliExpress

---
+++ AliExpress/app/src/main/java/ru/freeit/aliexpress/MainActivity.kt
@@ -0,0 +1,16 @@
+package ru.freeit.aliexpress
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.WebView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val webView = findViewById<WebView>(R.id.ali_express_web_view)
+        webView.loadUrl("aliexpress.ru")
+        webView.settings.javaScriptEnabled = true
+    }
+}
