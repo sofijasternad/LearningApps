160708177ede6344939385406464d6f6ee077ebc
Dmitry <dmitry.tehnicov@gmail.com>
2021-11-03 14:32:32 +0600

    создание обертки CoroutineViewModel

--- Notes/app/src/main/java/ru/freeit/notes/presentation/screens/notes/NotesViewModel.kt
+++ Notes/app/src/main/java/ru/freeit/notes/presentation/screens/notes/NotesViewModel.kt
@@ -1,14 +1,18 @@
 package ru.freeit.notes.presentation.screens.notes
 
+import kotlinx.coroutines.launch
+import ru.freeit.notes.core.CoroutineViewModel
 import ru.freeit.notes.core.LiveDataWrapper
 import ru.freeit.notes.data.db.AppDatabase
 import ru.freeit.notes.domain.entity.Note
 
-class NotesViewModel(private val database: AppDatabase) {
+class NotesViewModel(private val database: AppDatabase) : CoroutineViewModel() {
 
     private val notes = LiveDataWrapper<List<Note>>()
 
     fun init() {
+        viewModelScope.launch {
 
+        }
     }
 }
