80453e252fbe5c3ad050bc5e7b90d934306da5c8
Dmitry <dmitry.tehnicov@gmail.com>
2021-11-03 13:50:15 +0600

    создание приложения Notes

--- PersonalApp/app/src/main/java/ru/freeit/personalapp/RoundedImageView.kt
+++ PersonalApp/app/src/main/java/ru/freeit/personalapp/RoundedImageView.kt
@@ -1,11 +1,11 @@
 package ru.freeit.personalapp
 
 import android.content.Context
-import android.graphics.Canvas
-import android.graphics.Path
-import android.graphics.RectF
+import android.graphics.*
+import android.graphics.drawable.BitmapDrawable
 import android.util.AttributeSet
 import androidx.appcompat.widget.AppCompatImageView
+import kotlin.math.round
 
 class RoundedImageView @JvmOverloads constructor(
     ctx: Context,
@@ -13,19 +13,37 @@ class RoundedImageView @JvmOverloads constructor(
     defStyleAttr: Int = 0
 ) : AppCompatImageView(ctx, attrs, defStyleAttr) {
 
-    private fun Int.dp() = context.resources.displayMetrics.density * this
+    private val Int.dp
+        get() = context.resources.displayMetrics.density * this
 
-    override fun draw(canvas: Canvas) {
-
-        val path = Path().apply {
-            val rectF = RectF(0f, 0f, width.toFloat(), height.toFloat())
-            val radius = 100.dp()
-            addRoundRect(rectF, radius, radius, Path.Direction.CW)
-        }
-
-        canvas.clipPath(path)
-
-        super.draw(canvas)
+    private val roundedBitmapPaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        color = 0xff424242.toInt()
     }
+
+    override fun onDraw(canvas: Canvas) {
+        val drawable = drawable ?: return
+        if (width == 0 || height == 0) {
+            return
+        }
+        val bitmap = (drawable as BitmapDrawable).bitmap
+        val roundedBitmap = rounded(bitmap, 100.dp)
+        canvas.drawBitmap(roundedBitmap, 0f, 0f, null)
+    }
+
+    private fun rounded(bitmap: Bitmap, radius: Float) : Bitmap {
+        val result = Bitmap.createBitmap(bitmap.width, bitmap.height, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(result)
+        canvas.drawARGB(0, 0, 0, 0)
+
+        val rectF = RectF(0f, 0f, bitmap.width / 1f, bitmap.height / 1f)
+        canvas.drawRoundRect(rectF,  radius, radius, roundedBitmapPaint)
+        roundedBitmapPaint.xfermode = PorterDuffXfermode(PorterDuff.Mode.SRC_IN)
+
+        val rect = Rect(0, 0, bitmap.width, bitmap.height)
+        canvas.drawBitmap(bitmap, rect, rect, roundedBitmapPaint)
+
+        return result
+    }
+
 
 }
