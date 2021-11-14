a9a0151b1af0f33f065ba7152b0a59e37c8f0e12
Dmitry <dmitry.tehnicov@gmail.com>
2021-11-14 10:09:51 +0600

    изменение внешнего вида приложения и добавления названия главы

--- HarryPotter/app/src/main/java/ru/freeit/hiltapp/MainActivity.kt
+++ HarryPotter/app/src/main/java/ru/freeit/hiltapp/MainActivity.kt
@@ -5,20 +5,17 @@ import android.os.Bundle
 import android.widget.TextView
 import androidx.activity.viewModels
 import dagger.hilt.android.AndroidEntryPoint
-import ru.freeit.hiltapp.domain.repository.ChapterRepository
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-//    @Inject lateinit var repo: ChapterRepository
     private val viewModel by viewModels<MainViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val text = findViewById<TextView>(R.id.text)
+        val text = findViewById<TextView>(R.id.chapter_content)
 
         viewModel.observe(this) { content ->
             text.text = content
